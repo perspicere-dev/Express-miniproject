2677a906d8193f3f06e2381ae9527e8bd2f13cfe
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-29 17:44:14 +0100

    add handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
     res.show = (name) => {
@@ -25,7 +29,7 @@ app.use((req, res, next) => {
   });
 
   app.get('/hello/:name', (req, res) => {
-    res.send(`Hello ${req.params.name}!`);
+    res.render('hello', { layout: false, name: req.params.name }); //Funkcja render ma dwa parametry. Pierwszy ustala nazwę widoku, który chcemy wykorzystać, a drugi przekazuje obiekt z wartościami dla placeholderów
   });
 
   app.use((req, res) => {
